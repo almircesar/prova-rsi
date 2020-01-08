c05bc74568c8df34cd44f0e00f81b5d9537b334e
almir.damiao <almir.damiao@RSI1527.rsinet.com.br>
2020-01-08 16:15:27 -0300

    Classe cadastro finalizada

--- src/cucumberTest/SeleniumTest.java
+++ src/cucumberTest/SeleniumTest.java
@@ -3,6 +3,7 @@ package cucumberTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumTest {
 
@@ -33,11 +34,11 @@ public class SeleniumTest {
 		
 		Thread.sleep(2000);
 		
-		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[1]/div/input")).sendKeys("25082000abc");
+		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[1]/div/input")).sendKeys("25082000abC");
 		
 		Thread.sleep(2000);
 		
-		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[2]/div/input")).sendKeys("25082000abc");
+		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[1]/div[2]/sec-view[2]/div/input")).sendKeys("25082000abC");
 		
 		System.out.println("account details successful");
 		
@@ -45,17 +46,33 @@ public class SeleniumTest {
 		
 		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[2]/div[1]/sec-view[2]/div/input")).sendKeys("Damião");
 		
+		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[2]/div[2]/sec-view/div/input")).sendKeys("(11)98411-4939");
 	    
 		System.out.println("cadastro de detalhes feito com sucesso!!!!!");
 		
 		Thread.sleep(2000);
 		
+		//para selecionar caixa de seleção (tipo vários países)
 		
-
+        Select oSelect = new Select(driver.findElement(By.name("countryListboxRegisterPage")));
+        oSelect.selectByVisibleText("Brazil");
+        
+        driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[3]/div[1]/sec-view[2]/div/input")).sendKeys("Barueri");
 		
-		
-//		driver.quit();
+        driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[3]/div[2]/sec-view[1]/div/input")).sendKeys("Parque viana,rua jambeiro");
+        
+        driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[3]/div[2]/sec-view[2]/div/input")).sendKeys("São Paulo");
+        
+        driver.findElement(By.xpath("//*[@id=\"formCover\"]/div[3]/div[3]/sec-view/div/input")).sendKeys("São Paulo");
+      
+    	driver.findElement(By.xpath("//*[@id=\"formCover\"]/sec-view/div/input")).click();
+    	
+    	driver.findElement(By.xpath("//*[@id=\"register_btnundefined\"]")).click();
+    	System.out.println("Teste feito com sucesso");
+    	
+//  		driver.quit();
 		
 		
 	}
